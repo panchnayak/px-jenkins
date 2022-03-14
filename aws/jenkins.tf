ab569ac04ff497f6d80953b9e3fcb2b71484647d
Panchaleswar Nayak <panchaleswar@outlook.com>
2022-03-13 21:59:25 -0400

    first commit

---
+++ aws/jenkins.tf
@@ -0,0 +1,106 @@
+
+data "aws_ami" "centos" {
+owners      = ["679593333241"]
+most_recent = true
+
+  filter {
+      name   = "name"
+      values = ["CentOS Linux 7 x86_64 HVM EBS *"]
+  }
+
+  filter {
+      name   = "architecture"
+      values = ["x86_64"]
+  }
+
+  filter {
+      name   = "root-device-type"
+      values = ["ebs"]
+  }
+}
+
+resource "aws_key_pair" "ssh_key_pub" {
+   key_name = "${var.key_name}"
+   public_key = "${file("${var.key_path}/${var.key_pub}")}"
+}
+
+resource "aws_instance" "jenkins_instance" {
+  
+  # Lookup the correct AMI based on the region
+  # we specified
+  availability_zone      = "us-east-1a"
+  instance_type          = "${var.instance_type}"
+  ami                    = "${data.aws_ami.centos.id}"
+  key_name               = "${var.key_name}"
+  vpc_security_group_ids = ["${module.jenkins_sg.security_group_id}"]
+  subnet_id              = "${module.vpc.public_subnets[0]}"
+  associate_public_ip_address = true
+
+  # Our Security group to allow HTTP and SSH access
+  #user_data              = "${file("install_jenkins1.sh")}"
+  #Instance tags
+  root_block_device {
+  volume_type= "gp2"
+  volume_size= 50
+  }
+
+  tags = {
+    Name = "${var.vpc_name}-jenkins"
+    Terraform   = "true"
+    Environment = "dev"
+  }
+}
+
+resource "null_resource" "jenkins_admin_create" {
+
+  provisioner "file" {
+    source      = "./scripts"
+    destination = "/tmp"
+
+    connection {
+      user        = "${var.user_name}"
+      host        = "${aws_instance.jenkins_instance.public_ip}"
+      agent       = false
+      private_key = "${file("${var.key_path}/${var.key_private}")}"
+    }
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "sudo hostnamectl set-hostname jenkins-px",
+      "curl --silent --location http://pkg.jenkins.io/redhat-stable/jenkins.repo | sudo tee /etc/yum.repos.d/jenkins.repo",
+      "sudo rpm --import http://pkg.jenkins.io/redhat-stable/jenkins.io.key",
+      "sudo yum update -y",
+      "sudo yum install epel-release -y",
+      "sudo yum install java-11-openjdk-devel -y",
+      "sudo yum install daemonize jenkins -y",
+      "J_HOME=$(dirname $(dirname $(readlink $(readlink $(which javac)))))",
+      "sudo ln -s $J_HOME /usr/lib/jvm/openjdk11",
+      
+
+      #setting up JAVA_HOME path for all users
+      "export JAVA_HOME=/usr/lib/jvm/openjdk11",
+      "export CLASSPATH=$JAVA_HOME/lib/tools.jar:$JAVA_HOME/lib/dt.jar:.",
+      "export PATH=$PATH:$JAVA_HOME/bin:.",
+      "sudo systemctl start jenkins",
+      "sudo systemctl enable jenkins",
+
+      "sudo curl -sSL https://get.docker.com | bash",
+      "sudo usermod -a -G docker centos",
+      "sudo usermod -a -G docker jenkins",
+      "sudo systemctl enable docker --now",
+      "sudo yum install git -y",
+      "sudo mkdir -p /var/lib/jenkins/init.groovy.d",
+      "chmod +x /tmp/scripts/install_jenkins.sh" ,
+      "/tmp/scripts/install_jenkins.sh ${var.java_home} ${var.jenkins_username} ${var.jenkins_password}"
+    ]
+
+    connection {
+      user        = "${var.user_name}"
+      host        = "${aws_instance.jenkins_instance.public_ip}"
+      agent       = false
+      private_key = "${file("${var.key_path}/${var.key_private}")}"
+    }
+
+  }
+}
---
+++ aws/variables.tf
@@ -0,0 +1,97 @@
+## If you change this variable, You have to change in your AWS credential file:
+variable "vpc_name" {
+  default = "px-jenkins-vpc"
+}
+
+variable "key_name" {
+  default = "jenkins-keypair"
+}
+
+variable "region" {
+  default = "us-east-1"
+}
+
+variable "azs" {
+  default = ["us-east-1a"]
+  type    = list
+}
+
+variable "env" {
+  default = "dev"
+}
+
+variable "vpc_cidr" {
+  default = "10.0.0.0/16"
+}
+
+variable "volume_size" {
+  default = 15
+}
+
+variable "aws_ami_id" {
+  default = "ami-033b95fb8079dc481"
+}
+
+variable "instance_type" {
+  default = "t3.large"
+}
+
+variable "user_name" {
+  type = string
+  default = "centos"
+}
+
+variable "key_pub" {
+  default = "id_rsa.pub"
+}
+
+variable "key_private" {
+  default = "id_rsa"
+}
+
+variable "key_path" {
+  default = "~/.ssh"
+}
+
+variable "jenkins_sg" {
+  default = "jenkins-server-sg"
+}
+
+variable "admin_user" {
+  default = "pnayak"
+}
+
+variable "admin_password" {
+  default = "adminPassword"
+}
+
+variable "admin_full_name" {
+  default = "Panchaleswar Nayak"
+}
+
+variable "admin_email" {
+  default = "pnayak@purestorage.com"
+}
+
+variable "curlimage" {
+  default = "appropriate/curl"
+}
+
+variable "jqimage" {
+  default = "stedolan/jq"
+}
+
+
+variable "java_home" {
+  default = "/usr/lib/jvm/openjdk11"
+}
+
+variable "jenkins_username" {
+  default = "admin"
+}
+
+variable "jenkins_password" {
+  default = "password"
+}
+
+
