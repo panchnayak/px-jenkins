a1ef94f5e1012c4e8437b383e278cb12e648066e
Panchaleswar Nayak <panchaleswar@outlook.com>
2022-03-17 20:57:20 -0400

    updated

--- scripts/init-jenkins.sh
+++ scripts/init-jenkins.sh
@@ -4,4 +4,5 @@
 sudo cp /tmp/scripts/jenkins-create-admin.groovy /var/lib/jenkins/init.groovy.d
 #sudo sed -i '3,5 s/1.0/2.0/g' /var/lib/jenkins/config.xml
 sudo sed -i -e 's/<jdks\/>/<jdks> <jdk> <name>java-11-openjdk<\/name> <home>\/usr\/lib\/jvm\/openjdk11<\/home>  <properties\/> <\/jdk> <\/jdks>/g' /var/lib/jenkins/config.xml
+curl -LO http://localhost:8080/jnlpJars/jenkins-cli.jar
 sudo systemctl restart jenkins
