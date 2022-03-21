e6a9776d3c433b889049c35288088d563e6ba33a
panchnayak <panchaleswar@outlook.com>
2022-03-21 10:51:10 -0400

    updated

--- scripts/install-pipeline.sh
+++ scripts/install-pipeline.sh
@@ -3,7 +3,8 @@
 
 java -jar jenkins-cli.jar -auth $1:$2 -s http://localhost:8080  create-job eks-px-pipeline < /tmp/scripts/eks-px-pipeline.xml
 java -jar jenkins-cli.jar -auth $1:$2 -s http://localhost:8080  create-job gke-px-pipeline < /tmp/scripts/gke-px-pipeline.xml
-java -jar jenkins-cli.jar -auth $1:$2 -s http://localhost:8080  create-job update-argocd-password < /tmp/scripts/update-argocd-pass.xml
+java -jar jenkins-cli.jar -auth $1:$2 -s http://localhost:8080  create-job update-argocd-password < /tmp/scripts/argocd-pipeline.xml
+java -jar jenkins-cli.jar -auth $1:$2 -s http://localhost:8080  create-job update-argocd-password < /tmp/scripts/cassandra-db-pipeline.xml
 
 #sudo systemctl restart jenkins
 
